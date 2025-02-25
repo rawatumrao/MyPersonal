518119d3b625be2fdcd2a1d10d7a78a7ceb44dff
rawatumrao <rawatumrao@gmail.com>
2025-02-25 11:26:07 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import GlobalMeetUI from './components/GlobalMeetUI';
+import './assets/styles.css'; // Optional: Add custom styles
+
+function App() {
+    return (
+        <div className="App">
+            <GlobalMeetUI />
+        </div>
+    );
+}
+
+export default App;
